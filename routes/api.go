4371563b32bdb813f8179cc1cd48ca088414fc87
lianggf01 <lianggf01@mingyuanyun.com>
2021-08-16 18:19:32 +0800

    [feat]优化路由组定义中间件

--- routes/api.go
+++ routes/api.go
@@ -3,13 +3,23 @@ package routes
 import (
 	"fmt"
 	"github.com/gorilla/mux"
+	"log"
 	"net/http"
 )
 
 // RegisterApiRoutes 注册Api相关路由
 func RegisterApiRoutes(router *mux.Router) {
 	// 简单的路由组: api
-	router.HandleFunc("/api/ping", func(w http.ResponseWriter, r *http.Request) {
-		_, _ = fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-	}).Methods("GET").Name("about")
+	apiRouter := router.PathPrefix("/api").Subrouter()
+	apiRouter.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			//可以加上中间件
+			log.Println(r.RequestURI)
+
+			next.ServeHTTP(w, r)
+		})
+	})
+	apiRouter.HandleFunc("/ping1", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = fmt.Fprintf(w, "Hello1, %s!", r.URL.Path[1:])
+	}).Methods("GET").Name("about1")
 }
--- routes/web.go
+++ routes/web.go
@@ -3,13 +3,23 @@ package routes
 import (
 	"fmt"
 	"github.com/gorilla/mux"
+	"log"
 	"net/http"
 )
 
 // RegisterWebRoutes 注册网页相关路由
 func RegisterWebRoutes(router *mux.Router) {
 	// 简单的路由组: v1
-	router.HandleFunc("/api/ping1", func(w http.ResponseWriter, r *http.Request) {
-		_, _ = fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	apiRouter := router.PathPrefix("").Subrouter()
+	apiRouter.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			//可以加上中间件
+			log.Println(r.RequestURI)
+
+			next.ServeHTTP(w, r)
+		})
+	})
+	apiRouter.HandleFunc("/ping1", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = fmt.Fprintf(w, "Hello1, %s!", r.URL.Path[1:])
 	}).Methods("GET").Name("about")
 }
